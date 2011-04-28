Perl/perl5

commit 4dc941f7cb795735b9458431b362c0fed23ae92b

make PL_charclass available to modules under Win32

embedvar.h
@@ -707,6 +707,8 @@
 #define PL_GYes			(my_vars->GYes)
 #define PL_appctx		(my_vars->Gappctx)
 #define PL_Gappctx		(my_vars->Gappctx)
+#define PL_charclass		(my_vars->Gcharclass)
+#define PL_Gcharclass		(my_vars->Gcharclass)
 #define PL_check		(my_vars->Gcheck)
 #define PL_Gcheck		(my_vars->Gcheck)
 #define PL_csighandlerp		(my_vars->Gcsighandlerp)
@@ -799,6 +801,7 @@
 #define PL_GNo			PL_No
 #define PL_GYes			PL_Yes
 #define PL_Gappctx		PL_appctx
+#define PL_Gcharclass		PL_charclass
 #define PL_Gcheck		PL_check
 #define PL_Gcsighandlerp	PL_csighandlerp
 #define PL_Gcurinterp		PL_curinterp

perlapi.h
@@ -111,6 +111,8 @@ END_EXTERN_C
 #define PL_Yes			(*Perl_GYes_ptr(NULL))
 #undef  PL_appctx
 #define PL_appctx		(*Perl_Gappctx_ptr(NULL))
+#undef  PL_charclass
+#define PL_charclass		(*Perl_Gcharclass_ptr(NULL))
 #undef  PL_check
 #define PL_check		(*Perl_Gcheck_ptr(NULL))
 #undef  PL_csighandlerp

perlvars.h
@@ -120,6 +120,7 @@ PERLVAR(Gwatch_pvx, char*)
 PERLVAR(Gppaddr, Perl_ppaddr_t*) /* or opcode.h */
 PERLVAR(Gcheck,  Perl_check_t *) /* or opcode.h */
 PERLVARA(Gfold_locale, 256, unsigned char) /* or perl.h */
+PERLVARA(Gcharclass, 256, U32)
 #endif
 
 #ifdef PERL_NEED_APPCTX

sartak/perl

commit a3e240aad207f2efa45e23b6d1bb71d13192e63c

Restore building with -DPERL_GLOBAL_STRUCT, broken since 4dc941f7cb795735.
As PL_charclass is a constant, it doesn't need to be accessed via the global
struct. It should be exported via globvar.sym, not PERLVARA() in perlvars.h

[With a PERVARA() it all compiles perfectly, once C<dVAR>s are added where
now needed, but the build loops forever because the (real) charclass array is
never initialised]

embedvar.h
@@ -715,8 +715,6 @@
 #define PL_GYes			(my_vars->GYes)
 #define PL_appctx		(my_vars->Gappctx)
 #define PL_Gappctx		(my_vars->Gappctx)
-#define PL_charclass		(my_vars->Gcharclass)
-#define PL_Gcharclass		(my_vars->Gcharclass)
 #define PL_check		(my_vars->Gcheck)
 #define PL_Gcheck		(my_vars->Gcheck)
 #define PL_csighandlerp		(my_vars->Gcsighandlerp)
@@ -807,7 +805,6 @@
 #define PL_GNo			PL_No
 #define PL_GYes			PL_Yes
 #define PL_Gappctx		PL_appctx
-#define PL_Gcharclass		PL_charclass
 #define PL_Gcheck		PL_check
 #define PL_Gcsighandlerp	PL_csighandlerp
 #define PL_Gcurinterp		PL_curinterp

globvar.sym
@@ -5,6 +5,7 @@
 
 bincompat_options
 block_type
+charclass
 check
 core_reg_engine
 fold

perlapi.h
@@ -111,8 +111,6 @@ END_EXTERN_C
 #define PL_Yes			(*Perl_GYes_ptr(NULL))
 #undef  PL_appctx
 #define PL_appctx		(*Perl_Gappctx_ptr(NULL))
-#undef  PL_charclass
-#define PL_charclass		(*Perl_Gcharclass_ptr(NULL))
 #undef  PL_check
 #define PL_check		(*Perl_Gcheck_ptr(NULL))
 #undef  PL_csighandlerp

perlvars.h
@@ -116,7 +116,6 @@ PERLVAR(Gwatch_pvx, char*)
 PERLVAR(Gppaddr, Perl_ppaddr_t*) /* or opcode.h */
 PERLVAR(Gcheck,  Perl_check_t *) /* or opcode.h */
 PERLVARA(Gfold_locale, 256, unsigned char) /* or perl.h */
-PERLVARA(Gcharclass, 256, U32)
 #endif
 
 #ifdef PERL_NEED_APPCTX